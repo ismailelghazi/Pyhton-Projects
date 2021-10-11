028e53cc69dcc7fe22796a4a65b11a6e03b9844b
Ismail El Ghazi <Ismail47el@gmail.com>
2021-10-11 17:31:47 +0100

    thanks

--- first reddit bot/reddit.py
+++ first reddit bot/reddit.py
@@ -1,8 +1,25 @@
 import praw
 reddit = praw.Reddit(
-    client_id="SI8pN3DSbt0zor",
-    client_secret="xaxkj7HNh8kwg8e5t4m6KvSrbTI",
-    password="1guiwevlfo00esyy",
-    user_agent="testscript by u/fakebot3",
-    username="fakebot3",
+    client_id='xCQgBw-kL-Dh6ceO12hLPg',
+    client_secret='CW9QeOa_VMhAmnRWDBjpwD_aaWYpMQ',
+    password='jtQy+*$&p^eYQP3',
+    user_agent='ismail',
+    username='ismailELghazi',
 )
+count = 0
+word = ["griffiths", "review", "course", "courses", "DDS"]
+subreddit = reddit.subreddit('GAMSAT')
+hot_python = subreddit.new()
+for subm in hot_python:
+    bool = True
+    subreddit_title = subm.title
+    subreddit_text = subm.selftext
+    for test in word:
+        if subreddit_title.find(test) != -1 or subreddit_text.find(test) != -1:
+            print("*******")
+            bool = False
+            break
+    if bool == True:
+        print("title :{} \ntext: {}\n".format(subm.title, subm.selftext[:10000000000000]))
+        count += 1
+print(count)
