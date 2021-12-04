7990322c9210f16915f9652daf69460b362c3475
Zagreus Laptop <elouahdizaki@gmail.com>
2021-12-04 21:09:11 +0100

    updated

--- reddit_comment/fisrt.py
+++ reddit_comment/fisrt.py
@@ -1,8 +1,10 @@
+import sys
 import time
 import praw
 from datetime import date
 import random
-
+import json
+import os
 reddit = praw.Reddit(
     client_id='tuxe1nwe4PIyxARKSCYfoA',
     client_secret='NGUmHTvdBDSBpvhBLpetFTyjJFdAxA',
@@ -18,28 +20,72 @@ SUB_NAME = 'how_want_comment_'
 
 def get_id_post():
     subreddit = reddit.subreddit(SUB_NAME)
-    submissions = subreddit.hot()
+    submissions = subreddit.new()
     list_reddit = []
     for submission in submissions:
         # date = datetime.datetime.fromtimestamp(submission.created_utc).strftime("%Y-%m-%d")
         # print(submission.title + "***" + submission.id + "***" + date)
         list_reddit.append(submission.id)
     return list_reddit
-print(get_id_post())
-get_id_post()
-def comment():
-    list_data = ["So Nice. Hope you also check out my NFT´s. I did serveral Collections. CryptobugsClub, CrypticBees, Coinimals and Hugs of Olaf. Let me know if you like them"
-                 ,"Hey, i am new in this. But i found an amazing Artist. Check out Secret NFT Artist on Instagram or Twitter.",
-                 "Is Elon Musk the Secret NFT Artist? I just saw this Account at Opensea and was wondering if he is behind this. Do you know?"
-                 ,"Check out my Buddy! Secret NFT Artist @Opensea is releasing his NFT´s",
-                 "NFT Giveaway!!! We are giving away a small part of our CrypticBee Swarm. All you have to do: 🐝 post your ETH Adress in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CrypticBees @opensea 💥good Luck",
-                 "Catch the Swarm. There are just 222 of these CrypticBees available. Minted by an algorithm 🚀💥🪐 Creator🤖 Secret NFT Artist. Sale at Opensea NFT Giveaway!!! We are giving away a small part of our CryptoBugsClub. All you have to do: 🐝 post your ETH Adresse in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CryptoBugsClub Items @opensea 💥good Luck"]
-    for ids in get_id_post():
-        comment_random = random.choice(list_data)
-        print(comment_random)
-        print("eee")
-        submission = reddit.submission(id=ids)
-        print("aaa")
-        submission.reply(comment_random)
+def dataJson():
+    with open('listId.txt' , 'w') as json_file:
+        json.dump(get_id_post() , json_file)
 
-comment()
+
+def comment():
+    try:
+        data = ''
+        with open(os.path.join(sys.path[0] , 'listId.txt') , 'r') as f:
+            data = json.load(f)
+        list_data = [
+            "So Nice. Hope you also check out my NFT´s. I did serveral Collections. CryptobugsClub, CrypticBees, Coinimals and Hugs of Olaf. Let me know if you like them"
+            ,
+            "Hey, i am new in this. But i found an amazing Artist. Check out Secret NFT Artist on Instagram or Twitter.",
+            "Is Elon Musk the Secret NFT Artist? I just saw this Account at Opensea and was wondering if he is behind this. Do you know?"
+            , "Check out my Buddy! Secret NFT Artist @Opensea is releasing his NFT´s",
+            "NFT Giveaway!!! We are giving away a small part of our CrypticBee Swarm. All you have to do: 🐝 post your ETH Adress in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CrypticBees @opensea 💥good Luck",
+            "Catch the Swarm. There are just 222 of these CrypticBees available. Minted by an algorithm 🚀💥🪐 Creator🤖 Secret NFT Artist. Sale at Opensea NFT Giveaway!!! We are giving away a small part of our CryptoBugsClub. All you have to do: 🐝 post your ETH Adresse in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CryptoBugsClub Items @opensea 💥good Luck"]
+        for ids in get_id_post():
+            test = False
+            for ids2 in data:
+                if ids != ids2:
+                    test = True
+                else:
+                    test = False
+                    break
+            if test:
+                comment_random = random.choice(list_data)
+                print(comment_random)
+                print("eee")
+                submission = reddit.submission(id=ids)
+                print("aaa")
+                submission.reply(comment_random)
+            else:
+                print('post already commented on')
+        dataJson()
+        print('second time gen file')
+
+    except:
+        list_data = [
+            "So Nice. Hope you also check out my NFT´s. I did serveral Collections. CryptobugsClub, CrypticBees, Coinimals and Hugs of Olaf. Let me know if you like them"
+            ,
+            "Hey, i am new in this. But i found an amazing Artist. Check out Secret NFT Artist on Instagram or Twitter.",
+            "Is Elon Musk the Secret NFT Artist? I just saw this Account at Opensea and was wondering if he is behind this. Do you know?"
+            , "Check out my Buddy! Secret NFT Artist @Opensea is releasing his NFT´s",
+            "NFT Giveaway!!! We are giving away a small part of our CrypticBee Swarm. All you have to do: 🐝 post your ETH Adress in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CrypticBees @opensea 💥good Luck",
+            "Catch the Swarm. There are just 222 of these CrypticBees available. Minted by an algorithm 🚀💥🪐 Creator🤖 Secret NFT Artist. Sale at Opensea NFT Giveaway!!! We are giving away a small part of our CryptoBugsClub. All you have to do: 🐝 post your ETH Adresse in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CryptoBugsClub Items @opensea 💥good Luck"]
+        for ids in get_id_post():
+                comment_random = random.choice(list_data)
+                print(comment_random)
+                print("eee")
+                submission = reddit.submission(id=ids)
+                print("aaa")
+                submission.reply(comment_random)
+        dataJson()
+        print('first time gen file')
+index = 0
+while True:
+    comment()
+    time.sleep(30)
+    index+= 1
+    print(index)
