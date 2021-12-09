40cf4e59f970b9545a46f7dc190ff7c72fbb7930
Ismail El Ghazi <Ismail47el@gmail.com>
2021-12-09 12:31:28 +0100

    redddit-vf

--- reddit_comment/fisrt.py
+++ reddit_comment/fisrt.py
@@ -6,38 +6,36 @@ import json
 import os
 import time
 
-reddit = praw.Reddit(
-    client_id='1py94aNGRxpn6jSG9yjiyw',
-    client_secret='Eo3nVnPFZkKfrTBm0QZm9cGljQGoPw',
-    password='Redditbot123',
-    user_agent='ismail1',
-    username='nfthomie',
-)
 today = date.today()
 d1 = today.strftime("%d/%m/%Y")
 print("d1 =", d1)
-
-
+subName = ''
 def get_id_post():
     global list_reddit
-    sub_name = ['how_want_comment_','NFTExchange','NFT','NFTsMarketplace','NFTArt_Finance','NFTmarket','opensea','CryptoCurrency']
+    sub_name = ['how_want_comment_','heeelolo']
     sub = random.choice(sub_name)
+    global subName
+    subName = sub
     subreddit = reddit.subreddit(sub)
     submissions = subreddit.new()
     list_reddit = []
     for submission in submissions:
-        list_reddit.append(submission.id,submission)
+        list_reddit.append(submission.id)
     return list_reddit
 
-def dataJson():
-    with open('listId.txt', 'w') as json_file:
-        json.dump(get_id_post(), json_file)
 
+def dataJson():
+    print(subName + ' this dumps data')
+    with open(subName+'listId.txt', 'w') as json_file:
+        json.dump(listReddit, json_file)
 
 def comment():
+    global listReddit
     try:
+        listReddit = get_id_post()
         data = ''
-        with open(os.path.join(sys.path[0], 'listId.txt'), 'r') as f:
+        print(subName + ' this gets data')
+        with open(os.path.join(sys.path[0], subName+'listId.txt'), 'r') as f:
             data = json.load(f)
         list_data = [
             "So Nice. Hope you also check out my NFT´s. I did serveral Collections. CryptobugsClub, CrypticBees, Coinimals and Hugs of Olaf. Let me know if you like them"
@@ -49,7 +47,7 @@ def comment():
             "Catch the Swarm. There are just 222 of these CrypticBees available. Minted by an algorithm 🚀💥🪐 Creator🤖 Secret NFT Artist. Sale at Opensea NFT Giveaway!!! We are giving away a small part of our CryptoBugsClub. All you have to do: 🐝 post your ETH Adresse in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CryptoBugsClub Items @opensea 💥good Luck",
             "Catch the Swarm. There are just 222 of these CrypticBees available. Minted by an algorithm 🚀💥🪐 Creator🤖 Secret NFT Artist. Sale at Opensea NFT Giveaway!!! We are giving away a small part of our CryptoBugsClub. All you have to do: 🐝 post your ETH Adresse in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CryptoBugsClub Items @opensea 💥good Luck",
             "Xmas is on the Way. I created a „Hugs of Olaf“ Collection. Check out the Happy & Scary Olafs @opensea. Gime a Hug and collect one. 🚀💥🪐"]
-        for ids in get_id_post():
+        for ids in listReddit:
             test = False
             for ids2 in data:
                 if ids != ids2:
@@ -58,19 +56,19 @@ def comment():
                     test = False
                     break
             if test:
+                print('commenting on a post in ' + subName)
                 comment_random = random.choice(list_data)
-                print(comment_random)
-                print("eee")
                 submission = reddit.submission(id=ids)
-                print("aaa")
-                time.sleep(500)
+                # time.sleep(250)
                 submission.reply(comment_random)
             else:
                 print('post already commented on')
         dataJson()
-        print('second time gen file')
+        print('second time gen file ' + subName)
 
     except:
+        listReddit = get_id_post()
+        print(subName)
         list_data = [
             "So Nice. Hope you also check out my NFT´s. I did serveral Collections. CryptobugsClub, CrypticBees, Coinimals and Hugs of Olaf. Let me know if you like them"
             ,
@@ -79,21 +77,18 @@ def comment():
             , "Check out my Buddy! Secret NFT Artist @Opensea is releasing his NFT´s",
             "NFT Giveaway!!! We are giving away a small part of our CrypticBee Swarm. All you have to do: 🐝 post your ETH Adress in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CrypticBees @opensea 💥good Luck",
             "Catch the Swarm. There are just 222 of these CrypticBees available. Minted by an algorithm 🚀💥🪐 Creator🤖 Secret NFT Artist. Sale at Opensea NFT Giveaway!!! We are giving away a small part of our CryptoBugsClub. All you have to do: 🐝 post your ETH Adresse in this Post 🤖Follow @secret_nft_artist on Instagram 🚀Like at least 3 CryptoBugsClub Items @opensea 💥good Luck"]
-        for ids in get_id_post():
+        for ids in listReddit:
             comment_random = random.choice(list_data)
-            print(comment_random)
-            print("eee")
             submission = reddit.submission(id=ids)
-            print("aaa")
+            # time.sleep(250)
             submission.reply(comment_random)
         dataJson()
-        print('first time gen file')
+        print('first time gen file ' + subName)
 
 
 index = 0
 while True:
-    print(get_id_post())
-    time.sleep(100)
+    # time.sleep(100)
     comment()
     index += 1
     print(index)
